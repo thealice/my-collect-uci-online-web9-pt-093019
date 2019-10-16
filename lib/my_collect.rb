3da442397921f338c6d3e832c227270ae58acc2f
Alice Freda <alicefreda@gmail.com>
2019-10-16 19:02:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << yield(array[i])
+    # new_array << yield(array[i])
     i += 1
   end
-  new_array
+  # new_array
 end
